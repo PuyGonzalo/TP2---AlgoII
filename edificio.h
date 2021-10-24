85f6e37c8f6f239b132279faee2d67bc73470574
Puy Gonzalo <gpuy@fi.uba.ar>
2021-10-24 18:38:13 -0300

    Comienzo a trabajar en la clase edificio

--- edificio.h
+++ edificio.h
@@ -1,8 +1,11 @@
-#ifndef
+#ifndef EDIFICIO_H
 #define EDIFICIO_H
 
-#include <sstream>
 #include <string>
+#include <sstream>
+
+using namespace std;
+
 
 class Edificio{
 
@@ -10,11 +13,11 @@ private:
     string nombre;
     string identificador;
     string material_brindado;
-    int costo_piedra;
-    int costo_madera;
-    int costo_metal;
-    int cantidad_construidos;
-    int maximos_permitidos;
+    int costo_piedra{};
+    int costo_madera{};
+    int costo_metal{};
+    int cantidad_construidos{};
+    int maximos_permitidos{};
 
 public:
     Edificio();
@@ -22,6 +25,12 @@ public:
     int obtener_costo_piedra();
     int obtener_costo_madera();
     int obtener_costo_metal();
+    int obtener_maximos_permitidos();
+    string obtener_nombre();
+    string obtener_identificador();
+    //string obtener_material_brindado(); // DUDAS
+    void agregar_cantidad_construidos(); //Nombre prototipo, esta horrible eso xD.
+    void restar_cantidad_construidos();
 };
 
 #endif // EDFICIO_H
